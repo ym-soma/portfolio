11d7091b6290122a83d5c59b766eb621774e0e4d
yumi souma <yumi.souma1203@gmail.com>
2022-11-06 17:35:16 +0900

    サイト更新
    レイアウトの修正やjQueryの追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
+// スムーススクロール
+$(function () {
+  $("a[href^=#]:not([href=#])").click(function () {
 
-$(function(){
-  $("a[href^=#]:not([href=#])").click(function(){
+    var target = $($(this).attr("href")).offset().top;
 
-    var target=$($(this).attr("href")).offset().top;
-
-    $("html,body").animate({scrollTop:target},500);
+    $("html,body").animate({ scrollTop: target }, 500);
 
     return false;
+  });
+});
+
+
+// ナビゲーション、ハンバーガーボタン
+$(function () {
+  // ハンバーガーボタンをクリックしたら
+  $(".btn").click(function () {
+    // ボタンにクラス付与
+    $(this).toggleClass("active");
+    // ナビにクラス付与
+    $(".humberger").toggleClass("open");
+    // 背景のクラス付与
+    $(".nav_bg").toggleClass("huberger_bg");
+  });
+
+  // ナビのリンクをクリックしたら
+  $(".humberger a").click(function () {
+    // ボタンにクラス除去
+    $(".btn").removeClass("active");
+    // ナビにクラス除去
+    $(".humberger").toggleClass("open");
+    // 背景のクラス除去
+    $(".nav_bg").removeClass("huberger_bg");
   });
 });
